b360b3f0ee2527a51d4ef87b7a98a520e29abffe
hwaiplay <vs8170@naver.com>
2026-03-01 23:37:52 +0900

    Ignore application-loc.yml

---
+++ src/main/frontend/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, '') // 백엔드 API에 /api가 없다면 이 줄 추가
+      }
+    }
+  }
+})
