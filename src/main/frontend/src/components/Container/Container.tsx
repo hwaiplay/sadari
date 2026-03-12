aa88ae4a64d41c2070c6dcbc78847937889a5be2
Vella <112395421+vellahw@users.noreply.github.com>
2026-03-12 22:19:39 +0900

    26.03.12 컨테이너 컴포넌트 제작

--- src/main/frontend/src/components/Container/Container.tsx
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from "react";
-import { container } from "./container.css";
-
-export function Container({ children }: { children: ReactNode }) {
-  return <div className={container}>{children}</div>;
-}
