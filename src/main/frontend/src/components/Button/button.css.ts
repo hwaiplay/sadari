543a3bfc7ceaf8937c3d076273a28672ef8ea155
Vella <112395421+vellahw@users.noreply.github.com>
2026-03-12 22:19:32 +0900

    26.03.12 버튼 컴포넌트 제작

--- src/main/frontend/src/components/Button/button.css.ts
+++ src/main/frontend/src/components/Button/button.css.ts
@@ -2,16 +2,14 @@ import { style, styleVariants } from "@vanilla-extract/css";
 import { vars } from "../../styles/tokens.css";
 
 export const buttonBase = style({
-  height: "48px",
-  padding: `0 ${vars.space.lg}`,
+  padding: `0 ${vars.space.md}`,
   borderRadius: vars.radius.xl,
 
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
 
-  fontSize: vars.fontSize.body,
-  fontWeight: 700,
+  fontFamily: vars.font.heading,
   lineHeight: 1,
 
   border: "none",
@@ -27,5 +25,27 @@ export const buttonVariant = styleVariants({
   primary: {
     background: vars.color.black,
     color: "white",
+  },
+  secondary: {
+    background: vars.color.black025,
+    color: "white",
+  },
+});
+
+export const buttonSize = styleVariants({
+  sm: {
+    height: "32px",
+    padding: "0 12px",
+    fontSize: "14px",
+  },
+
+  md: {
+    height: "40px",
+    fontSize: "14px",
+  },
+
+  lg: {
+    height: "48px",
+    fontSize: "16px",
   },
 });
