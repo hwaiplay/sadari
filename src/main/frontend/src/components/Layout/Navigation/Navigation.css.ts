96cc4a385b7f168861dcf3525a2e5aef6a073121
Vella <112395421+vellahw@users.noreply.github.com>
2026-03-13 23:19:44 +0900

    26.03.13 하단 바 컴포넌트

--- src/main/frontend/src/components/Layout/Navigation/Navigation.css.ts
+++ src/main/frontend/src/components/Layout/Navigation/Navigation.css.ts
@@ -1,16 +1,25 @@
 import { style } from "@vanilla-extract/css";
+import { vars } from "../../../styles/tokens.css";
+
+export const navContainer = style({
+  position: "fixed",
+  bottom: 0,
+  right: 0,
+  left: 0,
+  zIndex: 997,
+  // background: "#ffffffb9",
+  width: "100%",
+  height: vars.headerHeight,
+  margin: "0 auto",
+  display: "flex",
+  alignItems: "center",
+});
 
 export const navigation = style({
   display: "flex",
   justifyContent: "space-between",
   alignItems: "center",
-  height: "60px",
-  zIndex: 997,
-  position: "fixed",
-  bottom: 0,
-  left: 0,
-  margin: 0,
-  background: "#e0e0e080",
+  width: "100%",
 });
 
 export const navButton = style({
--- src/main/frontend/src/components/Layout/Navigation/Navigation.tsx
+++ src/main/frontend/src/components/Layout/Navigation/Navigation.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { Button } from "../../Button/Button";
 import { useNavigate } from "react-router-dom";
-import { navButton, navigation } from "./Navigation.css";
-import clsx from "clsx";
-import { container } from "../Container/container.css";
+import { navButton, navContainer, navigation } from "./Navigation.css";
+import { Container } from "../Container/Container";
 function Navigation() {
   const navigate = useNavigate();
   //홈 버튼 네비게이트
@@ -21,17 +20,23 @@ function Navigation() {
     navigate("/mypage");
   };
   return (
-    <nav className={clsx(navigation, container)}>
-      <Button onClick={HomeOnClick} variant="secondary" className={navButton}>
-        홈
-      </Button>
-      <Button onClick={AddOnClick} className={navButton}>
-        기록하기
-      </Button>
-      <Button onClick={MypageOnClick} variant="secondary" className={navButton}>
-        마이페이지
-      </Button>
-    </nav>
+    <Container className={navContainer}>
+      <nav className={navigation}>
+        <Button onClick={HomeOnClick} variant="secondary" className={navButton}>
+          홈
+        </Button>
+        <Button onClick={AddOnClick} className={navButton}>
+          기록하기
+        </Button>
+        <Button
+          onClick={MypageOnClick}
+          variant="secondary"
+          className={navButton}
+        >
+          마이페이지
+        </Button>
+      </nav>
+    </Container>
   );
 }
 
