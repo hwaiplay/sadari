dcac99745d0969b1459d6c849c158019970e155f
Vella <112395421+vellahw@users.noreply.github.com>
2026-03-13 23:20:36 +0900

    26.03.13 Book 컴포넌트

---
+++ src/main/frontend/src/components/Book/book.css.ts
@@ -0,0 +1,43 @@
+import { style } from "@vanilla-extract/css";
+import { vars } from "../../styles/tokens.css";
+import { breakpoints } from "../../styles/breakpoints";
+
+export const book = style({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  textOrientation: "mixed",
+  fontFamily: vars.font.body,
+  padding: `${vars.space.md} 0`,
+
+  "@media": {
+    [`screen and (max-width: 767px)`]: {
+      height: vars.bookHeight.sm,
+      borderRadius: vars.radius.sm,
+    },
+    [`screen and (min-width: ${breakpoints.tablet}px)`]: {
+      height: vars.bookHeight.md,
+      borderRadius: vars.radius.md,
+    },
+  },
+});
+
+export const title = style({
+  fontSize: "1em",
+  writingMode: "vertical-rl",
+  letterSpacing: "-1px",
+  textAlign: "center",
+  height: "100%",
+  overflow: "hidden",
+});
+
+export const author = style({
+  fontSize: "11px",
+  marginTop: "8px",
+  position: "absolute",
+});
+
+export const tilt = style({
+  transform: "rotate(8deg) translateY(-1px) translateX(-16px)",
+});
