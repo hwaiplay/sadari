de5d0e96a42c453ac15e28ce56e71f1971e282e7
hwaiplay <vs8170@naver.com>
2026-03-16 00:41:56 +0900

    카카오 로그인 상수 추가

---
+++ src/main/java/org/our/sadari/constant/AuthConstant.java
@@ -0,0 +1,11 @@
+package org.our.sadari.constant;
+
+public class AuthConstant {
+
+    public static final String GRANT_TYPE = "grant_type";
+    public static final String CLIENT_ID = "client_id";
+    public static final String REDIRECT_URI = "redirect_uri";
+    public static final String CODE = "code";
+    public static final String AUTHORIZATION_CODE = "authorization_code";
+    public static final String AUTHORIZATION_URL = "https://kauth.kakao.com/oauth/token";
+}
