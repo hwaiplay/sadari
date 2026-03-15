591ba4c6d92de7dbbf651c438edeeca079108b02
Vella <gksdnjs2@gmail.com>
2026-03-15 18:37:28 +0900

    25.03.15 로그인 페이지

--- src/main/frontend/src/App.tsx
+++ src/main/frontend/src/App.tsx
@@ -1,13 +1,17 @@
 // App.tsx
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import MainLayout from "./components/Layout/MainLayout/MainLayout";
 import BookDetail from "./pages/BookDetail/BookDetail";
+import Login from "./pages/Login/Login";
 export default function App() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/home" element={<Home />} />
         <Route path="/detail/:id" element={<BookDetail />} />
       </Route>
     </Routes>
