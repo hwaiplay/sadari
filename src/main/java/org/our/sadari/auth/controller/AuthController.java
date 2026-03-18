6dcd0242c825640b6eb0e50738241d8852191011
Vella <112395421+vellahw@users.noreply.github.com>
2026-03-18 22:53:17 +0900

    feat: 카카오 로그인 API

--- src/main/java/org/our/sadari/auth/controller/AuthController.java
+++ src/main/java/org/our/sadari/auth/controller/AuthController.java
@@ -1,10 +1,13 @@
 package org.our.sadari.auth.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.our.sadari.auth.service.AuthServiceImpl;
 import org.our.sadari.auth.vo.KakaoAccountVO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,7 +22,12 @@ public class AuthController {
     private final AuthServiceImpl authServiceImpl;
 
     @GetMapping("/callback/kakao")
-    public KakaoAccountVO kakaoAuthLogin (@RequestParam("code") String code) throws JsonProcessingException {
-        return authServiceImpl.getKakaoToken(code);
+    public void kakaoAuthLogin (@RequestParam("code") String code, HttpServletResponse response) throws Exception {
+        // return authServiceImpl.kakaoLogin(code);
+        String token = authServiceImpl.kakaoLogin(code);
+
+        response.sendRedirect(
+            "http://localhost:5173/oauth?token=" + token
+        );
     }
 }
