7a0c3673b8177a264a2028074a1df376e88bab1c
Vella <112395421+vellahw@users.noreply.github.com>
2026-03-19 22:00:12 +0900

    style: 로그인 페이지 레이아웃 분리

--- src/main/frontend/src/pages/Login/Login.tsx
+++ src/main/frontend/src/pages/Login/Login.tsx
@@ -1,7 +1,16 @@
+/**
+ * fileName       : Login
+ * author         : Hanwon.Jang
+ * date           : 2026-03-19
+ * description    : 로그인 페이지
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2026-03-19        Hanwon.Jang       주석 추가
+ */
+
 import React from "react";
-import Header from "../../components/Layout/Header/Header";
 import { Link } from "react-router-dom";
-import axios from "axios";
 
 function Login() {
   const REST_API_KEY = import.meta.env.VITE_KAKAO_REST_API_KEY;
@@ -9,13 +18,11 @@ function Login() {
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=profile_nickname,profile_image`;
 
   return (
-    <div>
-      <Header />
-      <main style={{ marginTop: "100px" }}>
-        <h1>로그인</h1>
-        <Link to={KAKAO_AUTH_URL}>카카오로 3초만에 시작하기</Link>
-      </main>
-    </div>
+    <main style={{ marginTop: "100px" }}>
+      <img src={"/img/common/logo-b.svg"} alt="사다리 로고" />
+      <h1>로그인</h1>
+      <Link to={KAKAO_AUTH_URL}>카카오로 3초만에 시작하기</Link>
+    </main>
   );
 }
 
