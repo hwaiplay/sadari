154379c585179eecf79ef264d26ff4723260dd4a
Vella <gksdnjs2@gmail.com>
2026-03-21 16:20:21 +0900

    style: header 레이아웃 수정

--- src/main/frontend/src/components/Layout/Header/Header.tsx
+++ src/main/frontend/src/components/Layout/Header/Header.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { header } from "./Header.css";
+import { header, logo } from "./Header.css";
 import { Container } from "../Container/Container";
 
 function Header() {
   return (
-    <Container className={header}>
-      <header>
-        <Link to="/">
+    <header>
+      <Container className={header}>
+        <Link to="/" className={logo}>
           <img src={"/img/common/logo-b.svg"} alt="사다리 로고" width={100} />
         </Link>
-      </header>
-    </Container>
+      </Container>
+    </header>
   );
 }
 
