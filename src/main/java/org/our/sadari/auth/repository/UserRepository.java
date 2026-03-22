37364d1f4b8fd163c64c4b34b7f9ef69baa9b7b7
hwaiplay <vs81702738@gmail.com>
2026-03-22 14:14:06 +0900

    2026.03.22 로그인 인증 인가 구조변경

--- src/main/java/org/our/sadari/auth/repository/UserRepository.java
+++ src/main/java/org/our/sadari/auth/repository/UserRepository.java
@@ -16,5 +16,5 @@ import java.util.Optional;
  */
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
-    Optional<UserEntity> findByProviderId(String provierId);
+    Optional<UserEntity> findByUserIdxx(String provierId);
 }
