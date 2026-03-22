aa6e9cdec8e11c6c5158294b348a1df2e65c87a4
hwaiplay <vs8170@naver.com>
2026-03-22 21:47:09 +0900

    2026.03.22 엑세스 토큰, 리프레시 토큰 발급 완료 및 로그 데이터 적재 성공

---
+++ src/main/java/org/our/sadari/auth/entity/TokenHistoryEntity.java
@@ -0,0 +1,59 @@
+package org.our.sadari.auth.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+/**
+ * fileName       : TokenHistoryDto
+ * author         : SeungHyeon.Kang
+ * date           : 2026-03-22
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2026-03-22        SeungHyeon.Kang       최초 생성
+ */
+@Entity
+@Table(name = "TB_TOKHIS")
+public class TokenHistoryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tokhis_seq_gen")
+    @SequenceGenerator(
+            name = "tokhis_seq_gen",
+            sequenceName = "TOKHIS_SEQ",
+            allocationSize = 1
+    )
+    private Long id;
+
+    // 유저와 연결
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_NUMB", nullable = false)
+    private UserEntity userNumb;
+
+    // refreshToken
+    @Column(name = "REFR_TOKN", nullable = false, unique = true, length = 500)
+    private String refrTokn;
+
+    // 만료 시간
+    @Column(name = "EXPR_DATE", nullable = false)
+    private LocalDateTime exprDate;
+
+    // 생성 시간
+    @Column(name = "CRET_DATE", nullable = false)
+    private LocalDateTime cretDate;
+
+    protected TokenHistoryEntity() {}
+
+    public TokenHistoryEntity(UserEntity userNumb, String refrTokn, LocalDateTime exprDate) {
+        this.userNumb = userNumb;
+        this.refrTokn = refrTokn;
+        this.exprDate = exprDate;
+        this.cretDate = LocalDateTime.now();
+    }
+
+    public boolean isExpired() {
+        return exprDate.isBefore(LocalDateTime.now());
+    }
+}
