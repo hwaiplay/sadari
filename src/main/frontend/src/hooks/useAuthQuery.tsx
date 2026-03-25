294670a08e0570f8dc7090327e7d68acd1fbcbea
Vella <gksdnjs2@gmail.com>
2026-03-25 12:19:02 +0900

    feat: 로그인 검증 로직 수정중

--- src/main/frontend/src/hooks/useAuthQuery.tsx
+++ src/main/frontend/src/hooks/useAuthQuery.tsx
@@ -1,13 +1,29 @@
 import { useQuery } from "@tanstack/react-query";
 import api from "../api/axios";
 
+/**
+ * fileName       : useAuthQuery
+ * author         : hanwon.Jang
+ * date           : 2026-03-25
+ * description    : 인증 상태를 조회하는 커스텀 훅
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2026-03-24       hanwon.Jang       최초 생성
+ */
+
 export const useAuthQuery = () => {
   return useQuery({
     queryKey: ["auth"],
     queryFn: async () => {
-      const res = await api.get("/oauth/me");
-      return res;
+      try {
+        const res = await api.get("/oauth/tokenCheck");
+        // console.log("성공:", res);
+        return res.data;
+      } catch (err) {
+        console.log("에러 발생:", err);
+        throw err; // 중요 (Query가 에러 인식하도록)
+      }
     },
-    retry: false, // 실패 시 재시도 ❌
   });
 };
