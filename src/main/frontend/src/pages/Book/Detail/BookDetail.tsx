f870f1c316304cabf421ad57b26e1c963338d5ef
Vella <112395421+vellahw@users.noreply.github.com>
2026-05-02 15:53:23 +0900

    feat: 독후감 수정 작업중

--- src/main/frontend/src/pages/Book/Detail/BookDetail.tsx
+++ src/main/frontend/src/pages/Book/Detail/BookDetail.tsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useBookDetail } from "@/features/Book/Detail/hook/useBookDetail";
 import Loading from "@/components/Loading/Loading";
 import { Container } from "@/components/Layout/Container/Container";
+import { setReport } from "@/features/Book/api/bookApi";
 
 function BookDetail() {
   const { id } = useParams();
@@ -26,9 +27,17 @@ function BookDetail() {
   // 독후감 데이터
   const bookData = data?.data[0];
 
+  // 수정버튼
+  const setReportBtn = () => {
+    setReport(bookData?.reportNumb);
+  };
+
   return data?.code === 200 && bookData ? (
     // {bookData.map(book =>()}
     <Container>
+      <h1 onClick={setReportBtn} style={{ backgroundColor: "#e3d3d3" }}>
+        독후감 수정하기
+      </h1>
       <div>
         <h3>{bookData.bookTitl}</h3>
       </div>
