1286bc0f4a04fefcdedbe386b3272074993c4120
Vella <gksdnjs2@gmail.com>
2026-05-04 15:12:13 +0900

    feat: mybatis 마이그레이션 중

---
+++ src/main/java/org/our/sadari/sadariUser/user/mapper/UserMapper.java
@@ -0,0 +1,27 @@
+package org.our.sadari.sadariUser.user.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.our.sadari.sadariUser.user.dto.UserDto;
+
+/**
+ * packageName    : org.our.sadari.sadariUser.mapper
+ * fileName       : UserMapper
+ * author         : hanwon.Jang
+ * date           : 2026-05-03
+ * description    : 사용자 관련 mapper
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2026-05-03       hanwon.Jang       최초 생성
+ */
+
+@Mapper
+public interface UserMapper {
+    /**
+     * 유저 조회
+     * @param req
+     * @return
+     */
+    UserDto getUser(UserDto req);
+    
+}
