07b50a828753fc73ec20b3002c2b82931975c072
hwaiplay <vs8170@naver.com>
2026-07-24 16:20:43 +0900

    feat: pwa 오픈

--- src/main/frontend/src/main.tsx
+++ src/main/frontend/src/main.tsx
@@ -2,9 +2,13 @@
 import "./app/styles/global.css";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
+import { registerServiceWorker } from "./app/pwa/registerServiceWorker";
 import App from "./App";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
 );
+
+registerServiceWorker();
