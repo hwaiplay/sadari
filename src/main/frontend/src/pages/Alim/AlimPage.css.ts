aec63615b6e696575e2ab5cc6c3a9fe33e626bde
vella <112395421+vellahw@users.noreply.github.com>
2026-07-25 21:57:55 +0900

    feat: 알림 페이지

--- src/main/frontend/src/pages/Alim/AlimPage.css.ts
+++ src/main/frontend/src/pages/Alim/AlimPage.css.ts
@@ -6,7 +6,7 @@ export const page = style({
   maxWidth: "600px",
   minHeight: "100svh",
   margin: "0 auto",
-  padding: "82px 18px 96px",
+  padding: "20px 18px 96px",
   backgroundColor: "#ffffff",
   boxSizing: "border-box",
 });
@@ -158,10 +158,11 @@ export const itemTitle = style({
 
 export const itemContent = style({
   margin: 0,
-  fontFamily: vars.font.body,
-  fontSize: "12px",
+  fontFamily: vars.font.semibold,
+  fontSize: "14px",
   lineHeight: 1.45,
-  color: "#555555",
+  letterSpacing: '-1.4px',
+  color: vars.color.black,
 });
 
 export const itemDate = style({
