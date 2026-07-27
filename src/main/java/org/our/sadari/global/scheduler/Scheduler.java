cb4b2e336eb6cc6d6065fb98cf3adf8cff97c1d9
hwaiplay <vs8170@naver.com>
2026-07-27 11:48:57 +0900

    fix: 스케줄러 시간 변경

--- src/main/java/org/our/sadari/global/scheduler/Scheduler.java
+++ src/main/java/org/our/sadari/global/scheduler/Scheduler.java
@@ -33,9 +33,9 @@ public class Scheduler {
      *
      * @author Seunghyeon.Kang
      */
-//    @Schedules({@Scheduled(cron = "0 */5 9 * * *"), // 오전 9:00 ~ 9:55 (5분 간격)
-//                @Scheduled(cron = "0 0 10 * * *")})  // 오전 10:00 정각
-    @Scheduled(fixedRate = 10000)
+    @Schedules({@Scheduled(cron = "0 */5 9 * * *"), // 오전 9:00 ~ 9:55 (5분 간격)
+                @Scheduled(cron = "0 0 10 * * *")})  // 오전 10:00 정각
+    //@Scheduled(fixedRate = 10000) //테스트
     public void sendReportDateOverAlim() {
         /*
          * CodeUtil은 USEE_YSNO가 Y인 상세코드만 반환한다.
@@ -50,4 +50,5 @@ public class Scheduler {
 
         reportDateOverService.sendReportDateOverAlim();
     }
+
 }
