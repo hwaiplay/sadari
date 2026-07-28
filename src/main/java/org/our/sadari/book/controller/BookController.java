20d303ca89c8068086c37908ce69485ee62c29d9
hwaiplay <vs81702738@gmail.com>
2026-07-28 11:15:47 +0900

    refactor : 프로젝트 룰 재정의 교육

--- src/main/java/org/our/sadari/book/controller/BookController.java
+++ src/main/java/org/our/sadari/book/controller/BookController.java
@@ -29,17 +29,16 @@ public class BookController {
     private final ReportService reportService;
 
     /**
-     * searchBooks 메서드의 요청을 검증하고 업무 처리 결과를 반환한다.
+     * 검색어와 검색 시작 위치를 사용하여 네이버 도서 API의 도서 목록을 검색한다.
      *
      * @author Seunghyeon.Kang
-     * @return 처리 결과
+     * @return 검색된 도서 목록
      */
     @GetMapping("/search")
     @Operation(summary = "도서 검색", description = "네이버 도서 API를 사용해 사용자가 입력한 검색어로 도서를 조회한다.")
-    public ResultData searchBooks(@Parameter(description = "도서 검색어", example = "히가시노 게이고")
-                                  @RequestParam("query") String query
-                                , @Parameter(description = "네이버 검색 시작 위치", example = "1")
-                                  @RequestParam(value = "start", defaultValue = "1") int start) {
+    public ResultData searchBooks(@Parameter(description = "도서 검색어", example = "히가시노 게이고")@RequestParam("query") String query
+                                , @Parameter(description = "네이버 검색 시작 위치", example = "1")@RequestParam(value = "start", defaultValue = "1") int start) {
+
         return bookSearchService.searchBooks(query, start);
     }
 
@@ -51,8 +50,8 @@ public class BookController {
      */
     @GetMapping("/ratingAverage/by-isbn")
     @Operation(summary = "ISBN 공개 평점 평균 조회", description = "공개/비공개 여부와 관계없이 해당 ISBN으로 작성된 독후감 평점 평균을 조회한다.")
-    public ResultData getRatingAverageByIsbn(@Parameter(description = "평점 평균을 조회할 도서 ISBN", example = "9788972756194")
-                                             @RequestParam("isbn") String isbn) {
+    public ResultData getRatingAverageByIsbn(@Parameter(description = "평점 평균을 조회할 도서 ISBN", example = "9788972756194")@RequestParam("isbn") String isbn) {
+
         return reportService.getPublicRatingAverageByIsbn(isbn);
     }
 }
