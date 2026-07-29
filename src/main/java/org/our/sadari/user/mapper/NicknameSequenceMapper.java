411559cfd7ff429b8568bbbf1089157d2680a0e3
hwaiplay <vs81702738@gmail.com>
2026-07-29 15:17:01 +0900

    feat : 닉네임 정책 변경, 랜덤 닉네임 정책 도입

---
+++ src/main/java/org/our/sadari/user/mapper/NicknameSequenceMapper.java
@@ -0,0 +1,45 @@
+package org.our.sadari.user.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.our.sadari.user.dto.NicknameSequenceDto;
+
+/**
+ * fileName       : NicknameSequenceMapper
+ * author         : SeungHyeon.Kang
+ * date           : 2026-07-29
+ * description    : 신규 회원 닉네임 번호 발급 데이터 접근 메서드를 정의한다
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2026-07-29        SeungHyeon.Kang    최초 생성
+ */
+@Mapper
+public interface NicknameSequenceMapper {
+
+    /**
+     * 현재 연월의 닉네임 조합별 마지막 발급 번호를 증가시킨다
+     *
+     * @author SeungHyeon.Kang
+     * @param request 번호를 증가시킬 닉네임 조합
+     * @return 변경된 닉네임 번호 행 수
+     */
+    int uptNicknameSequence(NicknameSequenceDto request);
+
+    /**
+     * 현재 연월의 닉네임 조합별 최초 발급 번호를 등록한다
+     *
+     * @author SeungHyeon.Kang
+     * @param request 최초 번호를 등록할 닉네임 조합
+     * @return 등록된 닉네임 번호 행 수
+     */
+    int setNicknameSequence(NicknameSequenceDto request);
+
+    /**
+     * 현재 연월의 닉네임 조합별 마지막 발급 번호를 조회한다
+     *
+     * @author SeungHyeon.Kang
+     * @param request 조회할 닉네임 조합
+     * @return 현재 연월과 마지막 발급 번호
+     */
+    NicknameSequenceDto getNicknameSequenceDtl(NicknameSequenceDto request);
+}
